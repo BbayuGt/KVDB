daaadd235b5128834dd5053bdf3caedfb508f22e
BbayuGt <49893593+BbayuGt@users.noreply.github.com>
2026-05-19 17:55:13 +0700

    Better pattern matching

--- src/parser.rs
+++ src/parser.rs
@@ -20,41 +20,32 @@ pub fn parse(input: String) -> Result<Actions, String> {
      if input_splitted.len() < 2 {
          Err(String::from("Invalid Format"))
      } else {
-         let act: Actions;
-         
-         match input_splitted[0].to_uppercase().as_str() {
-             "GET" => {
-                 act = Actions {
+         let act: Actions = match input_splitted[0].to_uppercase().as_str() {
+             "GET" => Actions {
                      action: ActionsType::GET,
                      key: input_splitted[1].to_string(),
                      value: None
-                 };
-             }
-             "SET" => {
-                 act = Actions {
+                 },
+             "SET" => Actions {
                      action: ActionsType::SET,
                      key: input_splitted[1].to_string(),
                      value: Some(input_splitted[2..].join(" ").to_string())
-                 };
-             }
-            "UPDATE" => {
-                 act = Actions {
+                 },
+            "UPDATE" =>
+                 Actions {
                      action: ActionsType::UPDATE,
                      key: input_splitted[1].to_string(),
                      value: Some(input_splitted[2..].join(" ").to_string())
-                 };
-             }
-            "DELETE" => {
-                 act = Actions {
+                 },
+            "DELETE" => Actions {
                      action: ActionsType::DELETE,
                      key: input_splitted[1].to_string(),
                      value: None
-                 };
-             }
+                 },
              _ => {
                  return Err(String::from("Unknown Method"));
              }
-         }
+         };
          println!("{act:#?}");
          Ok(act)
      }
